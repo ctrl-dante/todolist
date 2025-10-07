3c2ad459be61d93364ea467ca314d4c821949ccb
ctrl-dante <agonbi@outlook.com>
2025-10-07 20:05:55 +0200

    feat: add delete dom opiton and array for storing toDos

--- src/index.js
+++ src/index.js
@@ -4,14 +4,40 @@ testLog();
 
 const body = document.body;
 
-// to do object 
 
-const toDoObject = class{
+const toDoListArray =(() => {
+
+    const toDoListArray = [];
+
+    const addToArray = (arrayItem) => {
+        toDoListArray.push(arrayItem);
+    };
+
+    //console.log(toDoListArray());
+
+    return {addToArray, toDoListArray,
+
+    }
+
+})();
+
+// to do object
+
+const toDoObject = class {
 
     constructor(title,description,dueDate,priority,project) {
          if(!new.target){
         throw Error("Not an object use new")
-    } 
+    }
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.project = project;
+    }
+
+    edit(title,description,dueDate,priority,project){
+
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
@@ -21,11 +47,13 @@ const toDoObject = class{
 
     //maybe the display should not be on the to do constructor
     displayToDoConsole(){
+
         console.log(this.title);
         console.log(this.description);
         console.log(this.dueDate);
         console.log(this.priority);
         console.log(this.project);
+
     };
 
     isComplete(){
@@ -37,17 +65,12 @@ const toDoObject = class{
         //delete to do
     }
 
-    edit(){
-        //edit to do
-    }
 
 }
 
 //const todo1 = new toDoObject("note","firstNote","tommorrow Hopefully","High","Personal");
 
 //console.log(todo1.displayToDo());
-
-
 
 const dialog = document.querySelector("dialog");
 const showBtn = document.querySelector("dialog , .new"); // can select with css selectors
@@ -60,24 +83,24 @@ deleteBtn.addEventListener('click', () => {
     container.innerText = "";
 })
 
-
 showBtn.addEventListener('click', () => {
     dialog.showModal();
     console.log("show dialog")
-    
-});
 
+});
 
 closeBtn.addEventListener('click', () => {
     dialog.close();
     console.log("closed dialog")
 });
 
-
 addBtn.addEventListener('click', () => {
 
 getToDo();
 displayToDo();
+toDoListArray.addToArray(getToDo());
+console.log(toDoListArray.toDoListArray);
+
 
 });
 
@@ -93,14 +116,50 @@ const getToDo = () => {
     const project = document.querySelector(".project").value;
 
         const toDo = new toDoObject(title,`Description: ${description}`,date,`Priority: ${priority}`,`Project: ${project}`);
-        toDo.displayToDoConsole();
-        // console.log("+++++++++++DEBBUGING+++++++++++");
-        // console.log(date.__proto__);
-        // console.log(date);
-        // console.log("+++++++++++++++++++++++++++++++");
+        
+        
+       // const toDoSerialized = JSON.stringify(toDo); // stringify to seralize
+        
+       // localStorage.setItem("toDo", toDoSerialized);
+
+       // console.log(localStorage.getItem("toDo"));
+
+       // const toDoDeserialized = JSON.parse(localStorage.getItem("toDo"));
+
+       // console.log(toDoDeserialized);
+
+       // toDo.displayToDoConsole();
+       // console.log("+++++++++++DEBBUGING+++++++++++");
+       // console.log(date.__proto__);
+       // console.log(date);
+       // console.log("+++++++++++++++++++++++++++++++");
 
     return toDo
 
+    };
+
+    const editToDo = () => {
+
+        const editedToDo = getToDo();
+
+        editedToDo.edit("longIsland","longIsland","longIsland","longIsland","longIsland",);
+
+       // console.log(getToDo().title);
+
+
+        // editedToDo.title = document.querySelector(".title").value
+        // editedToDo.description = document.querySelector(".description").value;
+        // editedToDo.date = document.querySelector('.date').value;
+        // editedToDo.priority = document.querySelector(".priority").value;
+        // editedToDo.project = document.querySelector(".project").value;
+
+        //const editedToDo = getToDo();
+
+        console.log(editedToDo.title);
+
+        //editedToDo  = toDoObject(title,`Description: ${description}`,date,`Priority: ${priority}`,`Project: ${project}`);
+
+        return editedToDo
     };
 
  const displayToDo = () => {
@@ -108,7 +167,13 @@ const getToDo = () => {
           const toDoElement = document.createElement("div");
           const toDoElementTitle = document.createElement("h2");
           const toDoElementDueDate = document.createElement("p");
-          const container = document.querySelector(".toDoDisplay")
+          const container = document.querySelector(".toDoDisplay");
+          const deleteToDoBtn = document.createElement("button");
+          const editToDoBtn = document.createElement("button");
+
+          deleteToDoBtn.innerText = "Delete To Do";
+          editToDoBtn.innerText = "Edit To Do";
+
 
           const toDo = getToDo();
 
@@ -118,38 +183,74 @@ const getToDo = () => {
 
           toDoElement.appendChild(toDoElementTitle);
           toDoElement.appendChild(toDoElementDueDate);
-          container.appendChild(toDoElement);
+          toDoElement.appendChild(deleteToDoBtn);
+          toDoElement.appendChild(editToDoBtn);
 
+          container.appendChild(toDoElement);
           body.appendChild(container);
 
         //   if(deleteToDo() < 1){
         //     deleteToDo();
         //   }
-        
-            deleteToDo();
-          
+
+        //remove to do
+        deleteToDoBtn.addEventListener('click', (e) => {
+
+            e.target.parentElement.remove();
+            //e.remove(e.parentElement);
+
+        });
+
+        //edit the content of to do
+        editToDoBtn.addEventListener('click', (e) => {
+            console.log(e.target.parentElement);
+            dialog.showModal();
+            //getToDo();
+            //showBtn.addEventListener('click', () => {
+            editToDo();
+            //console.log("show dialog")
+             //});
+        })
 
  };
 
- 
+ //STORE TO DO's in local STORAGE
 
-const deleteToDo = () => {
-
-    const toDoElement = document.querySelectorAll("div");
-
-    toDoElement.forEach(element => {
-            const deleteToDoBtn = document.createElement("button");
-            deleteToDoBtn.innerText = "Delete To Do";
-            element.appendChild(deleteToDoBtn);
-    })
-
- 
-
- }
+ //EDIT TO DO
 
 
 
-   deleteToDo.deleteBtn.addEventListener('click', () => {
-        parentElement.innerText = "";
-        console.log("delete");
-    })
+
+// const toDoElement = document.querySelector("div");
+
+
+// toDoElement.forEach(element => {
+//         const deleteToDoBtn = document.createElement("button");
+//         deleteToDoBtn.innerText = "Delete To Do";
+//         element.appendChild(deleteToDoBtn);
+// })
+
+
+//    deleteToDo.deleteBtn.addEventListener('click', () => {
+//         parentElement.innerText = "";
+//         //removeChild();
+//         console.log("delete");
+//     })
+
+
+//STORAGE EXERCISES
+
+
+// localStorage.setItem("testKey", "testValue");
+
+// sessionStorage.setItem("testSession", "testValue")
+// sessionStorage.setItem("testSession","Bob");
+
+
+// document.cookie = 'name=knexi; expires='+new Date(9999, 0, 1).toUTCString();
+
+
+// document.cookie = 'lastName=rasta; expires='+new Date(9999, 0, 1).toUTCString();
+
+// console.log(document.cookie);
+
